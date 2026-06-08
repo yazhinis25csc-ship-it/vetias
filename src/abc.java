73cfe7581211f199fc4221e1dbe62c982f199a76
yazhini <yazhinis25csc@vetias.ac.in>
2026-06-08 13:44:40 +0530

    abc

---
+++ src/abc.java
@@ -0,0 +1,9 @@
+void main(){
+    int n=5;
+    for(int row=1;row<=5;row++){
+        for(int column=1;column<=row;column++){
+            System.out.print();
+        }
+            System.out.println();
+    }
+}
