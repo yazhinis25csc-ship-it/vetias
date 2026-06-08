dd66618161c381b448b4562ba00e60d4cdd1b2af
yazhini <yazhinis25csc@vetias.ac.in>
2026-06-08 14:24:43 +0530

    code.java

--- src/code.java
+++ src/code.java
@@ -1,12 +1,3 @@
-/*void main(){
-    int n=5;
-    for(int row=1;row<=5;row++){
-        for(int column=1;column<=row;column++){
-            System.out.print(column);
-        }
-            System.out.println();
-    }
-}*/
 
     void main() {
         int[] marks = {87, 32, 44, 89, 23};
@@ -16,11 +7,12 @@
                 System.out.println(mark + "");
             }
         }
-        System.out.println("pass mark");
+        System.out.println("fail mark");
         for (int mark : marks) {
-            if (mark > 40) {
+            if (mark < 40) {
                 System.out.println(mark + "");
 
             }
         }
     }
+
