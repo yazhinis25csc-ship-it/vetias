5dc863cc8d695d0d2052ef2e046bf40564e6c374
yazhini <yazhinis25csc@vetias.ac.in>
2026-06-09 13:38:19 +0530

    Day4.java

--- src/Day4.java
+++ src/Day4.java
@@ -1,4 +1,4 @@
-void main(){
+/*void main(){
     int[][]marks=new int[5][3];
     int[][] studentmarks={
         {78,90,34},
@@ -13,4 +13,28 @@ void main(){
         }
         System.out.println();
     }
+}*/
+void main() {
+    int[][] studentattendance = {
+            {0, 1, 0},
+            {1, 0, 0},
+            {1, 1, 1},
+            {1, 0, 1},
+            {0, 0, 0}
+    };
+    for (int row = 0; row < studentattendance.length; row++) {
+        System.out.print ("student" + (row + 1) + ":");
+        for (int column = 0; column < studentattendance[row].length; column++) {
+            if (studentattendance[row][column] == 1) {
+                System.out.println("class" + (column + 1) + "Present");
+            } else {
+
+
+        }
+        System.out.println("class" + (column + 1) + ":Absent");
+
+    }
+    System.out.println(" ");
+
+}
 }
