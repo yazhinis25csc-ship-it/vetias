42756abdee69e919be5a66ba84dc7c3f7f24d0b2
yazhini <yazhini25csc@vetias.ac.in>
2026-06-18 09:33:51 +0530

    map

--- src/day11.java
+++ src/day11.java
@@ -1,6 +1,6 @@
 
-import java.util.HashSet;
-import java.util.Set;
+
+/*import java.util.Set;
  public class day11 {
     public static void main(String[]args){
         Set<String> set=new HashSet<>();
@@ -14,4 +14,21 @@ import java.util.Set;
             System.out.println(s);
         }
     }
+}*/
+import java.util.HashMap;
+public class day11 {
+    public static void main(String[]args) {
+        java.util.Map<String,Integer> mark=new HashMap<>();
+        mark.put("ram",89);
+        mark.put("john",78);
+        mark.put("aravindh",67);
+        mark.put("ramesh",90);
+        System.out.println(mark.get("percent"));
+        mark.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
 }
+
+
+
