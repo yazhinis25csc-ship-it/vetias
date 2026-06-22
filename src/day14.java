b97d2c92679e9633ced304227f312de55dca1a2a
yazhini <yazhini25csc@vetias.ac.in>
2026-06-22 12:09:28 +0530

    students

---
+++ src/day14.java
@@ -0,0 +1,45 @@
+static class Student {
+    String name;
+    int age;
+    int regNo;
+    Student(Builder builder) {
+        this.name = builder.name;
+        this.age = builder.age;
+        this.regNo = builder.regNo;
+    }
+    static class Builder {
+        public String name;
+        public int age;
+        public int regNo;
+
+        public Builder name(String nameValue) {
+            this.name = nameValue;
+            return this;
+        }
+        public Builder age(int ageValue) {
+            this.age = ageValue;
+            return this;
+        }
+        public Builder regNo(int regNoValue) {
+            this.regNo = regNoValue;
+            return this;
+        }
+        public Student build() {
+            return new Student(this);
+        }
+    }
+}
+static void main() {
+    // Student student = new Student("John",23, 425235, "21-12-5555",);
+    Student student = new Student.Builder()
+            .name("yazhu")
+            .age(18)
+            .regNo(24)
+            .build();
+
+    System.out.println(student.name);
+    System.out.println(student.age);
+    System.out.println(student.regNo);
+
+}
+
--- src/day9.java
+++ src/day9.java
@@ -1,14 +1,14 @@
 public class day9 {
-    static class college implements AutoCloseable {
+    static class college implements AutoCloseable{
         college(){
             System.out.println("college");
         }
         @Override
-        public void close() throws Exception {
+        public void close() throws Exception{
             System.out.println("college Close");
         }
     }
-    static class department extends college {
+    static class department extends college{
         department(){
             System.out.println("department");
         }
