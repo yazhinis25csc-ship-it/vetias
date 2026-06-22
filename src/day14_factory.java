b8a0c87d4fab46b8aa48d6e99efd767a974cff0a
yazhini <yazhini25csc@vetias.ac.in>
2026-06-22 14:59:00 +0530

    notification

--- src/day14_factory.java
+++ src/day14_factory.java
@@ -2,29 +2,29 @@ public class day14_factory {
     interface notification{
         void send(String to,String message);
     }
-    class Emailnotification implements notification{
+    class creditnotification implements notification{
         @Override
         public void send(String to,String message){
-            System.out.println("sending Email:"+message+",to"+to);
+            System.out.println("sending credit:"+message+",to"+to);
         }
     }
-    class Whatsappnotification implements notification{
+    class Debitnotification implements notification{
         @Override
         public void send(String to,String message){
-            System.out.println("sending whatsapp:"+message+",to"+to);
+            System.out.println("sending Debit:"+message+",to"+to);
         }
     }
     static notification getnotification(day14_factory factory,String type) {
         return switch (type) {
-            case "Email" -> factory.new Emailnotification();
-            case "whatsapp" -> factory.new Whatsappnotification();
+            case "credit" -> factory.new creditnotification();
+            case "Debit" -> factory.new Debitnotification();
             default -> throw new IllegalArgumentException("Invalid notification type: " + type);
         };
     }
     static void main(){
         day14_factory factory=new day14_factory();
-        String type="Email";
-        getnotification(factory,type).send("abc@gmail.com","program exceuted successfully");
+        String type="Debit";
+        getnotification(factory,type).send("abc@gmail.com","your account debit 800 rupee");
         }
         }
 
